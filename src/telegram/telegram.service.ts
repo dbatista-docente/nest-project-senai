41da0b1fdcadf3e7d59f9f3b82dae1cda2a68a8d
denerbatista <denerbatista@icloud.com>
2023-10-25 20:50:21 -0300

    feat: add service sendTelegrafMedia

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import {Telegraf} from 'telegraf';
+import { Telegraf } from 'telegraf';
+import { sendMessageDto } from './dto/sendMessage.dto';
+import { MediaGroup } from 'telegraf/typings/telegram-types';
 
 @Injectable()
 export class TelegramService {
@@ -10,14 +12,38 @@ export class TelegramService {
   //   this.bot = new Telegraf('6827435531:AAHgj-zB8zEenaJRUnrhRU0PBmmnxZUeQDU');
   //   this.chatId = '-1002033466946';
   // }
+  base64ToImage(base64: string): Buffer {
+    const base64Data = base64.replace(/^data:image\/jpeg;base64,/, '');
 
-  async sendTelegrafText({message, chatid, tokenbot }:{message: string, chatid: number, tokenbot: string }) {
+    return Buffer.from(base64Data);
+  }
+
+  async sendTelegrafText({ message, chatid, tokenbot }: sendMessageDto) {
     try {
       const bot = new Telegraf(tokenbot);
       await bot.telegram.sendMessage(chatid, message);
-      console.log("Mensagem enviada")
+      console.log('Mensagem enviada');
     } catch (error) {
-      console.log("sendTelegrafText: ", error)
+      console.log('sendTelegrafText: ', error);
     }
+  }
+
+  async sendTelegrafMedia(
+    message: string,
+    chatid: string,
+    tokenbot: string,
+    images: string[],
+  ) {
+    const bot = new Telegraf(tokenbot);
+    const media: MediaGroup = images.map((image) => ({
+      type: 'photo',
+      media: { source: this.base64ToImage(image) },
+      caption: '',
+    }));
+
+    media[media.length - 1].caption = message;
+
+    await bot.telegram.sendMediaGroup(chatid, media);
+    console.log('Mensagem enviada')
   }
 }
